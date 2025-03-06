bdcac321504c3a5f0fae86f541332d1260402dd2
Jeferson <jefeleal363@gmail.com>
2025-03-05 21:02:34 -0300

    feat: sucess in database conection

--- ws/database.js
+++ ws/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const URI = '';
+const URI =  "mongodb+srv://StudioUser:9FFB07troWV7SxJw@cluster0.5tel0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useCreateIndex", true);
+// mongoose.set("useUnifiedTopology", true);
 
 mongoose
     .connect(URI)
     .then(() => console.log('DB is Up'))
-    .catch(() => console.log(err));
+    .catch((err) => console.log(err));
